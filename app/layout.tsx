19b93c1fc3ea86be1e92cca75fe82153e673f3f1
Arvind Waghmare <Arvind.Waghmare@morningstar.com>
2023-10-03 15:25:52 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,38 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Navbar, Footer } from '@/components'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Navbar, Footer } from "@/components";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: 'Goa Beyond',
-  description: 'Luxurious Villa',
-}
+  title: "Goa Beyond",
+  description: "Luxurious Villa",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="relative">
         <Navbar></Navbar>
         {children}
-        <Footer></Footer>
+        <Footer></Footer> 
       </body>
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NNE2C915TF"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {
+        `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NNE2C915TF');
+        `}
+      </Script>
     </html>
-  )
+  );
 }
